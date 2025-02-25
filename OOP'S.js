e0aad841e104cfd70e5ed0912a7907700adf6239
Abdullah Siddiqui <abdullahsidzz333@gmail.com>
2025-02-25 16:18:22 +0500

    push

--- OOP'S.js
+++ OOP'S.js
@@ -1,25 +1,38 @@
+// CLASS 1
+// class Person {
+//   constructor(name,age){
+//       this.name = name
+//       this.age = age
 
-  
-  
-  class Person {
-    constructor(name,age){
-        this.name = name
-        this.age = age
+//   }
 
-    }
+//   getdetail(){
+//       return `My Name is ${this.name} and My Age is ${this.age}`
+//   }
+// }
+// let Person1 = new Person ("Abdullah",21)
+// let Person2= new Person ("Mohsin",20)
+// let Person3 = new Person ("Fabiha",18)
 
-    getdetail(){
-        return `My Name is ${this.name} and My Age is ${this.age}`
-    }
+// let outputVal = document.querySelector(".Output").innerHTML =`
+// ${Person1.getdetail()}<br>
+// ${Person2.getdetail()}<br>
+// ${Person3.getdetail()}
+
+//`
+
+// CLASS 2
+
+class Person {
+  constructor(name, age) {
+    (this.name = name), (this.age = age);
   }
-  let Person1 = new Person ("Abdullah",21)
-  let Person2= new Person ("Mohsin",20)
-  let Person3 = new Person ("Fabiha",18)
+  getDetail() {
+    console.log(`My Name is ${this.name} & My Age is ${this.age}`);
+  }
+}
 
-
-  let outputVal = document.querySelector(".Output").innerHTML =`
-  ${Person1.getdetail()}<br>
-  ${Person2.getdetail()}<br>
-  ${Person3.getdetail()}
-
-  `
+let Person1 = new Person("Abdullah", 21);
+let Person2 = new Person("Laiba", 20);
+Person1.getDetail();
+Person2.getDetail();
